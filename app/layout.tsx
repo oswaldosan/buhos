58681e9c7024c6028fcf53cd78e0324c2cfca06c
oswaldosanchez <mago.ads@gmail.com>
2025-01-17 16:24:36 -0600

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffbb00" />
+        <meta name="msapplication-TileColor" content="#061c2e" />
+        <meta name="theme-color" content="#061c2e" />
+      </head>
       <body className={roboto.className}>{children}</body>
     </html>
   );
